fae29207c5619d5a07421f0b4a83a9fc98381691
R7558 <rupsanaworld1004@gmail.co>
2023-07-29 16:57:54 +0600

    added update all

---
+++ src/data.js
@@ -0,0 +1,22 @@
+export const data = [
+    {
+      id: 1,
+      mainImage: "./components/asserts/images/image-product-1.jpg",
+      thumbnail: "./components/asserts/images/image-product-1-thumbnail.jpg",
+    },
+    {
+      id: 2,
+      mainImage: "./components/asserts/images/image-product-2.jpg",
+      thumbnail: "./components/asserts/images/image-product-2-thumbnail.jpg",
+    },
+    {
+      id: 3,
+      mainImage: "./components/asserts/images/image-product-3.jpg",
+      thumbnail: "./components/asserts/images/image-product-3-thumbnail.jpg",
+    },
+    {
+      id: 4,
+      mainImage: "./components/asserts/images/image-product-4.jpg",
+      thumbnail: "./components/asserts/images/image-product-4-thumbnail.jpg",
+    },
+  ];
